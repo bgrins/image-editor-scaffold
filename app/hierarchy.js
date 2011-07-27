5f7893dd85d739376e39e015799474569f59e9d9
bgrins <briangrinstead@gmail.com>
2011-07-27 17:00:28 -0500

    adding in some backbone for hierarchy

---
+++ app/hierarchy.js
@@ -0,0 +1,86 @@
+
+ProjectModel = Backbone.Model.extend({
+	initialize: function() {
+    	this.layers = new Layers;
+    	this.layers.url = '/repo/' + this.id + '/layers';
+	}
+});
+
+ProjectView = Backbone.View.extend({
+   	initialize: function(){
+		this.template = $("#search_template").html();
+		this.model.bind("all", this.render);
+		this.render();
+   	},
+   	render: function(){
+		var variables = { layers: this.model.layers.models };
+		log("Avout to render", variables);
+   		var compiled = _.template(this.template, variables);
+		this.el.html(compiled);
+   	},
+   	events: {
+   		"click input[type=button]": "addLayer"  
+   	},
+   	addLayer: function( event ){
+   		this.model.layers.add();
+		this.render();
+   	},
+   	updateCounts: function() {
+   
+   	}
+});
+
+
+log("Initializing layer model");
+LayerModel = Backbone.Model.extend({
+	initialize: function() {
+		var name = this.get("name");
+		if (!name) {
+			this.set({ "name": "Layer " + Math.random() });
+		}
+		
+		var items = new Items;
+		items.url = '/repo/' + this.id + '/items';
+		
+    	this.set({ "items": items });
+	}
+});
+
+LayerView = Backbone.View.extend({
+	initialize: function() {
+	
+	},
+	addItem: function( event ) {
+   		this.model.items.add();
+		
+	}
+});
+
+log("Initializing layers collection");
+Layers = Backbone.Collection.extend({
+	model: LayerModel,
+	initialize: function() {
+	
+	}
+});
+
+
+log("Initializing item model");
+ItemModel = Backbone.Model.extend({
+	initialize: function() {
+		var name = this.get("name");
+		if (!name) {
+			this.set({ "name": "Item " + Math.random() });
+		}
+		
+		log("Initialized item" , name)
+	}
+});
+
+log("Initializing items collection");
+Items = Backbone.Collection.extend({
+	model: ItemModel,
+	initialize: function() {
+		log("Initialized itm collection")
+	}
+});
--- img.js
+++ img.js
@@ -1,14 +1,4 @@
 
-window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(this.console) {
-      arguments.callee = arguments.callee.caller;
-      console.log( Array.prototype.slice.call(arguments) );
-  }
-};
-(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
-
 
 $(function() {
 	log("Loaded");
@@ -32,6 +22,13 @@ $(function() {
 		$(this).addClass("active");
 	});
 	controls.first().click();
+	
+	
+	window.project = new ProjectModel();
+	window.pview = new ProjectView({ 
+		model: project,
+		el: $("#layer_container") 
+	});
 });
 
 
